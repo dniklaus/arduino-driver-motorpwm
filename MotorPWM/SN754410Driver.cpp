76378801303695e2569dc09bb6c9f09f06016d15
dniklaus <dieter.niklaus@gmx.net>
2013-10-30 17:34:42 +0100

    - FIX: removed default pin assignments from SN754410Driver - FIX: SN754410Driver: avoid calling virtual method out of the ctor! - FIX: SN754410Driver: set the pins back to INPUT in the dtor

--- MotorPWM/SN754410Driver.cpp
+++ MotorPWM/SN754410Driver.cpp
@@ -7,27 +7,32 @@
 #include "Arduino.h"
 #include "SN754410Driver.h"
 
-
-const int SN754410_Driver::s_defaultHBridgeEnPin   = 44;
-const int SN754410_Driver::s_defaultHBridgeLeg1Pin = 46;
-const int SN754410_Driver::s_defaultHBridgeLeg2Pin = 48;
-
-
 SN754410_Driver::SN754410_Driver(int hBridgeEnPin, int hBridgeLeg1Pin, int hBridgeLeg2Pin)
 : MotorPWM()
 , m_hBridgeEnPin(hBridgeEnPin)
 , m_hBridgeLeg1Pin(hBridgeLeg1Pin)
 , m_hBridgeLeg2Pin(hBridgeLeg2Pin)
 {
-  pinMode(m_hBridgeEnPin,   OUTPUT); // PWM speed pin
+  pinMode(m_hBridgeEnPin,   OUTPUT);
   pinMode(m_hBridgeLeg1Pin, OUTPUT);
   pinMode(m_hBridgeLeg2Pin, OUTPUT);
 
-  updateActors();
+  digitalWrite(m_hBridgeLeg1Pin,  m_isDirFwd);
+  digitalWrite(m_hBridgeLeg2Pin, !m_isDirFwd);
+
+  // output speed as PWM value
+  analogWrite(m_hBridgeEnPin, m_speed);
 }
 
 SN754410_Driver::~SN754410_Driver()
-{ }
+{
+  m_speed = 0;
+  analogWrite(m_hBridgeEnPin, m_speed);
+
+  pinMode(m_hBridgeEnPin,   INPUT);
+  pinMode(m_hBridgeLeg1Pin, INPUT);
+  pinMode(m_hBridgeLeg2Pin, INPUT);
+}
 
 void SN754410_Driver::updateActors()
 {
--- MotorPWM/SN754410Driver.h
+++ MotorPWM/SN754410Driver.h
@@ -13,22 +13,22 @@
 /*
  *
  */
-class SN754410_Driver: public MotorPWM
+class SN754410_Driver : public MotorPWM
 {
 public:
-  SN754410_Driver(int hBridgeEnPin = s_defaultHBridgeEnPin, int hBridgeLeg1Pin = s_defaultHBridgeLeg1Pin, int hBridgeLeg2Pin = s_defaultHBridgeLeg2Pin);
+  SN754410_Driver(int hBridgeEnPin, int hBridgeLeg1Pin, int hBridgeLeg2Pin);
   virtual ~SN754410_Driver();
 
   virtual void updateActors();
 
 private:
-  int m_hBridgeEnPin;
+  int m_hBridgeEnPin;     // PWM speed pin
   int m_hBridgeLeg1Pin;
   int m_hBridgeLeg2Pin;
 
-  static const int s_defaultHBridgeEnPin;
-  static const int s_defaultHBridgeLeg1Pin;
-  static const int s_defaultHBridgeLeg2Pin;
+private: // forbidden default functions
+  SN754410_Driver& operator = (const SN754410_Driver& );  // assignment operator
+  SN754410_Driver(const SN754410_Driver& src);            // copy constructor
 };
 
 #endif /* SN754410DRIVER_H_ */
